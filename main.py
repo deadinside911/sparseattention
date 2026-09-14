89c9f980c81ebbdd71c4e559c9cee811f48b718a
deadinside911 <halan.raghav@gmail.com>
2026-09-15 00:26:36 +0530

    Add mask

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ scores = query_matrix @ keys_matrix.T
 
 scores /= (D ** 0.5)
 
+mask = torch.ones(N, N)
+scores = scores.masked_fill(mask == 0, float("-inf"))
+
 attention_weights = torch.softmax(scores, dim=1)
 
 output = attention_weights @ values_matrix
 
-print(output)
+print("attention weights", attention_weights)
+print("output", output)
