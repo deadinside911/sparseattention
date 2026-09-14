9da952aa4581e183f9266e829dc00f46c703ce07
deadinside911 <halan.raghav@gmail.com>
2026-09-14 23:18:00 +0530

    Manually implement manual (dense) attention

---
+++ WRITEUP.md
@@ -0,0 +1,5 @@
+# writeup
+
+## 1.1 Implementing manual attention
+
+Implementing "scaled dot product attention" as described in [Attention is All You Need](https://arxiv.org/pdf/1706.03762), using PyTorch.
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import torch
 
-shape = (4, 4)
+N, D = (3, 3)
 
-tokens = torch.rand(shape)
-print(tokens)
+query_matrix = torch.rand(N, D)
+keys_matrix = torch.rand(N, D)
+values_matrix = torch.rand(N, D)
+
+scores = query_matrix @ keys_matrix.T
+
+scores /= (D ** 0.5)
+
+attention_weights = torch.softmax(scores, dim=1)
+
+output = attention_weights @ values_matrix
+
+print(output)
